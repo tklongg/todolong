c2b9caeb1a9dfe0aeb1c3919ba94d13733c3fa91
tklongg <klong31122001@gmail.com>
2024-02-05 11:53:29 +0700

    add widgets

--- lib/screens/mainscreen/main_screen.dart
+++ lib/screens/mainscreen/main_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:todolong/screens/today/today_screen.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -19,61 +21,98 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        controller: _pageController,
-        children: _screens,
-        onPageChanged: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          _pageController.animateToPage(
-            index,
-            duration: const Duration(milliseconds: 150),
-            curve: Curves.linearToEaseOut,
-          );
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.today),
-            label: 'Today',
+        body: PageView(
+          controller: _pageController,
+          children: _screens,
+          onPageChanged: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _currentIndex,
+          selectedItemColor: const Color(0xFFD74638),
+          selectedLabelStyle: const TextStyle(color: Color(0xFFD74638)),
+          onTap: (index) {
+            _pageController.animateToPage(
+              index,
+              duration: const Duration(milliseconds: 150),
+              curve: Curves.linearToEaseOut,
+            );
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: buildTodayIcon(),
+              label: 'Today',
+            ),
+            const BottomNavigationBarItem(
+              icon: Icon(Icons.inbox),
+              label: 'Inbox',
+            ),
+            const BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            const BottomNavigationBarItem(
+              icon: Icon(Icons.list_alt_rounded),
+              label: 'Browse',
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          shape: CircleBorder(
+              side: BorderSide(color: const Color.fromARGB(0, 255, 255, 255))),
+          backgroundColor: Color(0xFFD74638),
+          child: const Icon(Icons.add,color: Colors.white,),
+        ));
+  }
+
+  Widget buildTodayIcon() {
+    // Lấy ngày hiện tại
+    String currentDay = DateFormat.d().format(DateTime.now());
+    // String currentDay = '29';
+
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Icon(Icons.calendar_today),
+        Positioned(
+          child: Container(
+            padding: EdgeInsets.fromLTRB(2, 5, 2, 2),
+            decoration: BoxDecoration(
+              // color: Colors.red,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Text(
+              currentDay,
+              style: const TextStyle(
+                fontSize: 10,
+                color: Colors.black,
+              ),
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.inbox),
-            label: 'Inbox',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list_alt_rounded),
-            label: 'Browse',
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
 
-class TodayScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Today'),
-      ),
-      body: Center(
-        child: Text('Today Screen'),
-      ),
-    );
-  }
-}
+// class TodayScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Today'),
+//       ),
+//       body: Center(
+//         child: Text('Today Screen'),
+//       ),
+//     );
+//   }
+// }
 
 class InboxScreen extends StatelessWidget {
   @override
--- lib/screens/today/today_screen.dart
+++ lib/screens/today/today_screen.dart
@@ -8,9 +8,49 @@ class TodayScreen extends StatefulWidget {
 }
 
 class _TodayScreenState extends State<TodayScreen> {
-  
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return RefreshIndicator(
+      onRefresh: () async {
+        print(1);
+        await Future.delayed(const Duration(seconds: 1));
+      },
+      child: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.all(9.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              const Text(
+                'Today',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontFamily: 'GGX88HV',
+                  // fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Text(
+                'to todolong',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: 'SF_Regular',
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Date: ${DateTime.now().toString()}',
+                style: TextStyle(
+                  fontFamily: 'GGX88Reg_Light',
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
