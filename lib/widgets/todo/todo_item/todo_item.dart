97e8b9347b7f552c80961df0eb74a3f4dd37baa4
tklongg <klong31122001@gmail.com>
2024-02-17 16:07:09 +0700

    handle slide delete

--- lib/widgets/todo/todo_item/todo_item.dart
+++ lib/widgets/todo/todo_item/todo_item.dart
@@ -7,6 +7,7 @@ import 'package:todolong/providers/todo_provider.dart';
 
 import 'package:todolong/widgets/todo/todo_item/todo_priority_circle.dart';
 import 'package:todolong/widgets/todo/todo_modal.dart/todo_modal.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoItemWidget extends StatelessWidget {
   final Todo todo;
@@ -23,52 +24,78 @@ class TodoItemWidget extends StatelessWidget {
     this.onOpenModal,
     this.onCloseModal,
   }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-      leading: CupertinoButton(
-        padding: const EdgeInsets.all(0),
-        onPressed: () {},
-        child: TodoPriorityCircle(
-          prior: todo.priority,
-          handleClick: () {
-            Provider.of<TodoProvider>(context, listen: false)
-                .completeTodo(todo.id!);
-          },
+    return Slidable(
+        closeOnScroll: true,
+        endActionPane: ActionPane(
+          extentRatio: 0.2,
+          motion: const ScrollMotion(),
+          children: [
+            SlidableAction(
+              // An action can be bigger than the others.
+              onPressed: (context) {
+                Provider.of<TodoProvider>(context, listen: false)
+                    .deleteTodo(todo.id!);
+              },
+              backgroundColor: Color(0xFFD1453A),
+              foregroundColor: Colors.white,
+              icon: Icons.delete,
+              label: 'Delete',
+            ),
+            // SlidableAction(
+            //   onPressed: doNothing,
+            //   backgroundColor: Color(0xFF0392CF),
+            //   foregroundColor: Colors.white,
+            //   icon: Icons.save,
+            //   label: 'Save',
+            // ),
+          ],
         ),
-      ),
-      title: Text(
-        todo.title,
-        style: const TextStyle(
-          fontFamily: ".SF Pro Text",
-          fontSize: 20,
-        ),
-      ),
-      onTap: () {
-        if (onOpenModal != null) {
-          onOpenModal!();
-        }
-
-        showModalBottomSheet<void>(
-          isDismissible: true,
-          context: context,
-          isScrollControlled: true,
-          useRootNavigator: true,
-          enableDrag: true,
-          backgroundColor: Colors.transparent,
-          builder: (context) => PopScope(
-            child: TodoModal(id: todo.id!),
-            onPopInvoked: (didPop) {
-              // onCloseModal();
-              if (onCloseModal != null) {
-                onCloseModal!();
-              }
-            },
+        child: ListTile(
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+          leading: CupertinoButton(
+            padding: const EdgeInsets.all(0),
+            onPressed: () {},
+            child: TodoPriorityCircle(
+              prior: todo.priority,
+              handleClick: () {
+                Provider.of<TodoProvider>(context, listen: false)
+                    .completeTodo(todo.id!);
+              },
+            ),
           ),
-        );
-      },
-    );
+          title: Text(
+            todo.title,
+            style: const TextStyle(
+              fontFamily: ".SF Pro Text",
+              fontSize: 20,
+            ),
+          ),
+          onTap: () {
+            if (onOpenModal != null) {
+              onOpenModal!();
+            }
+
+            showModalBottomSheet<void>(
+              isDismissible: true,
+              context: context,
+              isScrollControlled: true,
+              useRootNavigator: true,
+              enableDrag: true,
+              backgroundColor: Colors.transparent,
+              builder: (context) => PopScope(
+                child: TodoModal(id: todo.id!),
+                onPopInvoked: (didPop) {
+                  // onCloseModal();
+                  if (onCloseModal != null) {
+                    onCloseModal!();
+                  }
+                },
+              ),
+            );
+          },
+        ));
   }
 }
